3b4e056770af6bd5304d4d0270425cca29111942
Dazaa1 <marouanelankaoui066@gmail.com>
2026-02-16 21:25:35 +0000

    Add submit and retrive articles endpoints, modify .gitignore

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from schemas import Article
 
 # init fastapi
 app = FastAPI()
 
-app.get('/checkhealth')
+articles = []
+
+@app.get('/checkhealth')
 def checkhealth():
     return {"value" : "checkhealth"}
 
+@app.post('/submit')
+def submit_article(article: Article):
+    articles.append({"title": article.title, "content": article.content, "tages": article.tags})
+    return {"ok": "submitted"}
+
+@app.get('/articles')
+def retrieve_articles():
+    return articles
