753556eb050aea42f8986cae145efb45e5b21442
Dazaa1 <marouanelankaoui066@gmail.com>
2026-02-16 23:17:45 +0000

    Add fitering through query parameters

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from schemas import Article
+from typing import List
 
 # init fastapi
 app = FastAPI()
@@ -12,9 +13,15 @@ def checkhealth():
 
 @app.post('/submit')
 def submit_article(article: Article):
-    articles.append({"title": article.title, "content": article.content, "tages": article.tags})
+    articles.append({"title": article.title, "content": article.content, "tags": article.tags})
     return {"ok": "submitted"}
 
-@app.get('/articles')
-def retrieve_articles():
+@app.get('/articles/')
+def retrieve_articles(tags: List[str] = Query(None)):
+    print(articles)
+    if tags:
+        for article in articles:
+            for tag in tags:
+                if tag in article["tags"]:
+                    return article
     return articles
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
+import uuid
+from typing import List
 
 
 class Article(BaseModel):
+    id: int = uuid.uuid4()
     title: str
     content: str
-    tags: list[str]
+    tags: List[str]
