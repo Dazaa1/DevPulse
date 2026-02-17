ab00b9a98b81d71a9eadfbecaa750f96008741e8
Dazaa1 <marouanelankaoui066@gmail.com>
2026-02-17 18:17:45 +0000

    Not fixed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from app.database import create_db_and_tables
 from app.routers.articles import router
+from sqlmodel import Session
 
 # init fastapi
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get('/checkhealth')
 def checkhealth():
--- app/routers/articles.py
+++ app/routers/articles.py
@@ -1,17 +1,30 @@
-from fastapi import Query, APIRouter, HTTPException
-from app.schemas import Article
+from typing import Annotated
+from fastapi import Query, APIRouter, HTTPException, Depends
+from ..database import Article
+from sqlalchemy.orm import Session
+from ..database import engine
+
 
 router = APIRouter()
 
 articles = []
 
+# ensuring that sessions are closed and opened per request, not sure yet what it means
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
 @router.post('/')
-def submit_article(article: Article):
-    articles.append({"title": article.title, "content": article.content, "tags": article.tags})
-    return {"ok": "submitted"}
+def submit_article(article: Article, session: Session):
+    session.add(article)
+    session.commit()
+    session.refresh(article)
+    return article
 
 @router.get('/{article_id}')
-def retrieve_articles(article_id: int, tags: list[str] = Query(None)):
+def retrieve_articles(db: Session, article_id: int, tags: list[str] = Query(None)):
     if article_id < 0 or article_id >= len(articles):
         raise HTTPException(status_code=404, detail="Article not found")
     
@@ -21,4 +34,4 @@ def retrieve_articles(article_id: int, tags: list[str] = Query(None)):
         if not any(tag in article.get("tags", []) for tag in tags):
             raise HTTPException(status_code=404, detail="Article found, but tags do not match")
             
-    return article
+    return db.query(article).all()
